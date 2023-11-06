8611141f49491f504d207971e22b07413fa4ce91
collinshoromba <collinserver001@gmail.com>
2023-11-06 23:26:51 +0200

    commit this

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            if (input == 0)
+                break;
+
+            numbers.Add(input);
+        }
+
+        if (numbers.Count > 0)
+        {
+            int sum = numbers.Sum();
+            double average = numbers.Average();
+            int max = numbers.Max();
+            int smallestPositive = numbers.Where(x => x > 0).DefaultIfEmpty(int.MaxValue).Min();
+            
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {max}");
+
+            if (smallestPositive != int.MaxValue)
+            {
+                Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+            }
+            
+            numbers.Sort();
+            Console.WriteLine("The sorted list is:");
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No numbers entered.");
+        }
     }
 }
