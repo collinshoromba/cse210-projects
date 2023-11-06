163b59411155e3e3d890215869e3e74686d356d3
collinshoromba <collinserver001@gmail.com>
2023-11-06 23:12:04 +0200

    committhis

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+      int percent = int.Parse(answer);     
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percent >=80)
+        {
+            letter = "B";
+        }
+
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percent >= 60)
+        {
+            letter = "C";
+        }
+
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}.");
+
+        if (percent >= 71)
+        {
+            Console.WriteLine("You passed");
+        }
+        else 
+        {
+            Console.WriteLine("Work a little bit harder");
+        }
     }
 }
