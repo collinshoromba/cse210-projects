f2010e69ee13491097a394f891161d0f48c61eed
collinshoromba <collinserver001@gmail.com>
2023-11-13 15:51:56 +0200

    COMMIT THIS

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,43 @@
+using System;
+class Journal
+{
+    private List<JournalEntry> entries = new List<JournalEntry>();
+
+    public void AddEntry(JournalEntry entry)
+    {
+        entries.Add(entry);
+    }
+
+    public void DisplayEntries()
+    {
+        foreach (var entry in entries)
+        {
+            Console.WriteLine(entry);
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter writer = new StreamWriter(filename))
+        {
+            foreach (var entry in entries)
+            {
+                writer.WriteLine($"{entry.Date}|{entry.Prompt}|{entry.Response}");
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        entries.Clear();
+        string[] lines = File.ReadAllLines(filename);
+        foreach (var line in lines)
+        {
+            string[] parts = line.Split('|');
+            if (parts.Length == 3)
+            {
+                entries.Add(new JournalEntry(parts[1], parts[2], parts[0]));
+            }
+        }
+    }
+}
---
+++ prove/Develop02/JournalEntry.cs
@@ -0,0 +1,21 @@
+using System;
+
+
+class JournalEntry
+{
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+    public string Date { get; set; }
+
+    public JournalEntry(string prompt, string response, string date)
+    {
+        Prompt = prompt;
+        Response = response;
+        Date = date;
+    }
+
+    public override string ToString()
+    {
+        return $"{Date}\nPrompt: {Prompt}\nResponse: {Response}\n";
+    }
+}
