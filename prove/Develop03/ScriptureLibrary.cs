73c04768e4c8f9a208a1bcea3a302e23f83c996f
collinshoromba <collinserver001@gmail.com>
2023-11-20 21:56:20 +0200

    commit this

--- prove/Develop03/ScriptureLibrary.cs
+++ prove/Develop03/ScriptureLibrary.cs
@@ -13,7 +13,7 @@ class ScriptureLibrary
     {
         // scriptures that the user will be able to play with and i can put as many scriptures in this section.
         scriptures.Add(new Scripture("1 Nephi 3:7", "And it came to pass that I, Nephi, said to my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them..."));
-        scriptures.Add(new Scripture("Doctrine & Covenants 11:21", "Trust in the Lord with all your heart..."));
+        scriptures.Add(new Scripture("Doctrine & Covenants 11:21", "Seek not to declare my word, but first seek to obtain my word..."));
     }
 
     public Scripture GetRandomScripture()
